dc30514c7acdc196e60ec0fb1a7accdeda26d621
nekova <nekova07@gmail.com>
2015-03-23 04:40:58 +0900

    Fix migration's bug

--- db/migrate/20150319090048_sorcery_core.rb
+++ db/migrate/20150319090048_sorcery_core.rb
@@ -1,13 +1,8 @@
 class SorceryCore < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
-      t.string :name
-
-      t.timestamps
-    end
+    add_column :users, :email, :string, :null => false
+    add_column :users, :crypted_password, :string
+    add_column :users, :salt, :string
 
     add_index :users, :email, unique: true
   end
