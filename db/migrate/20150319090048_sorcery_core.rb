5837d079f72a2d9e28120e2234851ddb71f7c805
nekova <nekova07@gmail.com>
2015-03-24 04:48:08 +0900

    Migrate db

--- db/migrate/20150319090048_sorcery_core.rb
+++ db/migrate/20150319090048_sorcery_core.rb
@@ -1,6 +1,6 @@
 class SorceryCore < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string, :null => false
+    add_column :users, :email, :string
     add_column :users, :crypted_password, :string
     add_column :users, :salt, :string
 
--- db/schema.rb
+++ db/schema.rb
@@ -37,12 +37,12 @@ ActiveRecord::Schema.define(version: 20150320164247) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                           null: false
+    t.string   "name"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.string   "reset_password_token"
