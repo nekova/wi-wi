ed804e00afe818e6a5a08ba8f2ee71604bac030c
nekova <nekova07@gmail.com>
2015-03-29 01:49:58 +0900

    Trim UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to @comment.post, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
