fb95927e8db407d2efd7a3fd6443b43123a42ac0
nekova <nekova07@gmail.com>
2015-04-01 19:43:44 +0900

    Fix Redcarpet options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,9 @@ module ApplicationHelper
   def markdown(text)
     unless @markdown
       renderer = Redcarpet::Render::OriginalHTML.new({
-        hard_wrap: true, filter_html: true, autolink: true,
-        no_intraemphasis: true, fenced_code: true, gh_blockcode: true
+        hard_wrap: true, filter_html: true, gh_blockcode: true
       })
-      @markdown = Redcarpet::Markdown.new(renderer)
+      @markdown = Redcarpet::Markdown.new(renderer, autolink: true, fenced_code_blocks: true)
     end
     @markdown.render(text).html_safe
   end
